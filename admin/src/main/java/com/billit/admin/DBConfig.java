5aee300c47bbd80550088dcec9ef3bb5365ff27b
BlanC <kks4517@naver.com>
2025-05-26 14:11:37 +0900

    Chore: 멀티 모듈 설정
    - api/admin/domain/core/infra 모듈로 분리

---
+++ admin/src/main/java/com/billit/admin/DBConfig.java
@@ -0,0 +1,11 @@
+package com.billit.admin;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EntityScan(basePackages = "com.billit.domain.entity")
+@EnableJpaRepositories(basePackages = "com.billit.domain.repository")
+public class DBConfig {
+}
